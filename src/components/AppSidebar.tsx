47aea34e2896722eaeafc49371dc53995a5f7111
shantanuk7 <shantanukulkarni365@gmail.com>
2026-03-27 13:18:08 +0530

    feat(LMS-53): add sidebar app header with title and icon

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -5,11 +5,19 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import { CalendarDays } from "lucide-react"
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <div className="flex gap-2 items-center p-3 border-b border-b-gray-700">
+          <div className="rounded-lg bg-(--technogise-blue) p-1">
+            <CalendarDays color="white" className="p-1"/>
+          </div>
+          <h1 className="text-lg text-white font-bold">EasyLeave</h1>
+        </div>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
