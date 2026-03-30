181952296c522bab1c10a4e67ce363ca61a54191
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-30 12:15:44 +0530

    feat(LMS-15): add generic Table component with dynamic columns and unit tests

---
+++ src/components/Table.tsx
@@ -0,0 +1,44 @@
+import React, { type ReactNode } from 'react'
+
+type Column<T> = {
+    header: string;
+    render: (value: T) => ReactNode
+};
+
+type TableProps<T> = {
+    data: T[]
+    columns: Column<T>[]
+};
+
+function Table<T>({ data, columns }: TableProps<T>): React.JSX.Element {
+  return (
+        <div className="w-full shadow-xs rounded-2xl border border-neutral-200 overflow-x-auto">
+            <table className="w-full text-sm text-left">
+                <thead className="text-sm text-body border-b border-neutral-200">
+                    <tr>
+                        {columns.map((col, index) => (
+                            <th key={index} className="px-3 md:px-6 py-3 font-semibold text-nowrap">
+                                {col.header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((value, index) => (
+                            <tr key={index} className="border-b last:border-b-0 border-neutral-200 cursor-pointer hover:bg-neutral-50">
+                                {columns.map((col, colIndex) => (
+                                    <td key={colIndex} className="px-3 md:px-6 py-4">
+                                        {col.render(value)}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Table
