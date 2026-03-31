6a17faf96fb568c3489ac2e9dd6ccb8bd95732f5
RajDeshmukh2001 <rajdd61@gmail.com>
2026-03-31 09:46:42 +0530

    feat(LMS-7): add Login component in App layout

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,21 @@
-import { render } from '@testing-library/react'
-import App from "./App"
-import { describe, test } from 'vitest'
-import { MemoryRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { describe, test } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
+
 const renderApp = () => {
-    render(
-        <MemoryRouter>
-            <App />
-        </MemoryRouter>
-    )
-}
-describe('App Component',()=>{
-    test("renders App component content",()=>{
-        renderApp()
-    })
-})
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+};
+
+describe('App Component', () => {
+  test('renders App component content', () => {
+    renderApp();
+
+    expect(screen.getByRole('heading', { name: /easyleave/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /sign in with google/i })).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './App.css';
 import AppRoutes from './routes/AppRoutes';
+import Login from './components/Login';
 
-const App = () : React.JSX.Element => {
-    return (
-        <AppRoutes />
-    )
-}
+const App = (): React.JSX.Element => {
+  return (
+    <>
+      <AppRoutes />
+      <main className="w-full h-screen flex items-center justify-center">
+        <Login />
+      </main>
+    </>
+  );
+};
 
 export default App;
