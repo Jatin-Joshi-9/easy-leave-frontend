f313baff90b7d5218d9641412d8ff6f1fd21ac10
shantanuk7 <shantanukulkarni365@gmail.com>
2026-04-02 08:51:28 +0530

    feat(LMS-12): add react hot toast Toaster component in main besides App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import App from '@/App.tsx';
-import { BrowserRouter } from 'react-router-dom'
-import { TooltipProvider } from './components/ui/tooltip.tsx'
+import { BrowserRouter } from 'react-router-dom';
+import { TooltipProvider } from './components/ui/tooltip.tsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TooltipProvider>
       <BrowserRouter>
         <App />
+        <Toaster position='top-right' />
       </BrowserRouter>
     </TooltipProvider>
   </StrictMode>,
