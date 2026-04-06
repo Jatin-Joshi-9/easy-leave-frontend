693e60329598a2dd3dbd58acdc3866d8e6a9ffd8
shantanuk7 <shantanukulkarni365@gmail.com>
2026-04-06 14:57:45 +0530

    chore(LMS-12): add XSRF-TOKEN header in axios instance request to add the csrf token to each request

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use((response) => response);
+axiosInstance.interceptors.request.use((config) => {
+  const csrfToken = Cookie.get('XSRF-TOKEN');
+
+  if (csrfToken) {
+    config.headers['X-XSRF-TOKEN'] = csrfToken;
+  }
+  return config;
+});
 
 export default axiosInstance;
