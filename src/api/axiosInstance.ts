6bcefa89aaf8286ce0236ca0a951824e675c1e28
Jatin Joshi <143538502+Jatin-Joshi-9@users.noreply.github.com>
2026-04-13 16:52:26 +0530

    random1

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,17 +2,19 @@ import axios from 'axios';
 import Cookie from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const csrfToken = Cookie.get('XSRF-TOKEN');
+  const csrfToken = Cookie.get('XSRF-TOKEN');
+  console.log(csrfToken);
 
-  if (csrfToken) {
-    config.headers['X-XSRF-TOKEN'] = csrfToken;
-  }
-  return config;
+  if (csrfToken) {
+    config.headers['X-XSRF-TOKEN'] = csrfToken;
+    console.log(config.headers['X-XSRF-TOKEN']);
+  }
+  return config;
 });
 
 export default axiosInstance;
