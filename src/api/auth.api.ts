4bd4d9d0425371c863637a46bd92c23c1c591e2f
Jatin Joshi <143538502+Jatin-Joshi-9@users.noreply.github.com>
2026-04-13 17:17:51 +0530

    Fix API endpoint for fetching authenticated user

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -3,7 +3,7 @@ import axiosInstance from './axiosInstance';
 import type { ApiResponse } from '@/types/response';
 
 export const getAuthenticatedUser = async (): Promise<User> => {
-  const { data } = await axiosInstance.get<ApiResponse<User>>(`/api/auth/me`);
+  const { data } = await axiosInstance.get<ApiResponse<User>>(`/auth/me`);
   if (!data.success) {
     console.error('Error fetching current user:', data.message);
     throw new Error(data.message || 'Failed to fetch current user');
